263e05bfd78ac7aa15c6ae0bc5979ac228f49f2b
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-25 03:54:57 -0500

    declare and assign variable for Konami Code

---
+++ app.js
@@ -0,0 +1,7 @@
+
+  // Konami Code: up, up, down, down, left, right, left, right, B, A
+  /*
+  There are a few different ways to do this:
+  */
+  
+ var konami = [ 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a' ];
