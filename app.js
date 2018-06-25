a77f31790ec597b99649a25974303e84dc96aca2
bmccaffrey <bryan.patrick.mccaffrey@gmail.com>
2018-06-25 04:42:33 -0500

    compare keys pressed against Konami code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+// Konami Code: up, up, down, down, left, right, left, right, B, A
 
-  // Konami Code: up, up, down, down, left, right, left, right, B, A
-  /*
-  There are a few different ways to do this:
-  */
-  
- var konami = [ 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a' ];
+/*
+There are a few different ways to do this:
+*/
 
- var keysPressed = [];
+var konami = [ 'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a' ];
+
+var keysPressed = [];
 
 window.addEventListener( 'keydown', ( e ) => {
   console.log( e.key );
@@ -14,4 +14,8 @@ window.addEventListener( 'keydown', ( e ) => {
   console.log( keysPressed );
   keysPressed.splice( ( -konami.length -1 ) , ( keysPressed.length - konami.length ) );
   console.log( keysPressed );
-})
+
+  if ( keysPressed.join( '' ) === konami.join( '' )) {
+    console.log( 'jackpot' );
+  }
+});
